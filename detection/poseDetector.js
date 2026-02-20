c5fb27f313259e92305d37fe65aa044549fead5c
langsard <164439543+langsard@users.noreply.github.com>
2026-02-20 19:41:07 +0700

    Update poseDetector.js

--- detection/poseDetector.js
+++ detection/poseDetector.js
@@ -7,12 +7,15 @@ export async function initDetector() {
   const model = poseDetection.SupportedModels.MoveNet;
 
   detector = await poseDetection.createDetector(model, {
-    modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING
+    modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER
   });
 }
 
 export async function detectPose(source) {
 
   const poses = await detector.estimatePoses(source);
+
+  if (!poses || poses.length === 0) return [];
+
   return poses[0].keypoints;
 }
